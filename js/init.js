7e24a2bbb9ca4ab43091b8507897a62d589d89fb
iryna <irinagosteva2014@gmail.com>
2017-06-11 17:05:26 +0300

    done

---
+++ js/init.js
@@ -0,0 +1,10 @@
+ ;(function ($){
+	 // init bPopup
+	 $('._js-btn-open-modal').on('click', function (){
+	 	 $('#modal-order').bPopup({
+	 	 	closeClass:'modal-сlose',
+	 	 });
+	 });
+	// init fromstaller
+	 $('select').styler();
+ })(jQuery);
